4fef5363b5c31c605c9759d9ca6d7a595e5f764d
NagaBabu S.V <nagbabu.sv@gmail.com>
2026-04-05 22:56:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
-from app.chart import generate_chart
-from app.ai_engine import analyze_chart
+from fastapi.middleware.cors import CORSMiddleware
+from chart import generate_chart
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "Astro AI Running"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/predict")
-def predict(year: int, month: int, day: int, hour: float, lat: float, lon: float):
-    chart = generate_chart(year, month, day, hour, lat, lon)
-    analysis = analyze_chart(chart)
-    return {"chart": chart, "analysis": analysis}
+def predict(
+    year:int,
+    month:int,
+    day:int,
+    hour:float,
+    lat:float,
+    lon:float
+):
+    # ✅ Always India timezone
+    timezone = "Asia/Kolkata"
+
+    return {
+        "chart": generate_chart(year, month, day, hour, lat, lon, timezone)
+    }
